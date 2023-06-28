ee2d48ee621589caf9b9b38915b8d5cc706549a7
sunnyrithvik <sribhashyamrithvik@gmail.com>
2023-06-28 18:10:28 +0530

    added app2

--- src/app2.js
+++ src/app2.js
@@ -1,5 +1,5 @@
+import "./index.css";
 import { useState } from "react";
-import "./styles.css";
 
 const faqs = [
   {
@@ -16,7 +16,7 @@ const faqs = [
   },
 ];
 
-export default function App() {
+export default function App2() {
   return (
     <div>
       <Accordion data={faqs} />
@@ -25,36 +25,53 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
-      {data.map((el, i) => (
-        <div>
-          <AccordionItem
-            title={el.title}
-            text={el.text}
-            num={i}
-            key={el.title}
-          />
-        </div>
+      {data.map((item, i) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={item.title}
+          num={i}
+          key={item.title}
+        >
+          {item.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Test 1"
+        num={22}
+        key="test 1"
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ title, num, onOpen, curOpen, children }) {
+  const isOpen = num === curOpen;
 
-  function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+  function handleClick() {
+    onOpen(isOpen ? null : num);
   }
 
   return (
-    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleClick}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
